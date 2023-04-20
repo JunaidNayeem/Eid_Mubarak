51136c10aff8d67bab1ac979a18fb58ea2bd9ce6
Junaid Nayeem <junaid.n@opensenselabs.com>
2023-04-20 23:50:19 +0530

    add bg

--- src/App.js
+++ src/App.js
@@ -3,26 +3,44 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div class="container-outer">
-        <div class="container">
-          <div class="main-text">
-            <div class="top">
-              <p>EID</p>
-            </div>
-            <div class="bottom">
-              <p>EID</p>
-            </div>
+      <div id="eid-greetings">
+        <div class="lanterns swingimage"></div>
+        <div class="lanterns swingimage-1"></div>
+        <div class="lanterns swingimage-2"></div>
+        <div class="lanterns swingimage-3"></div>
+        <div id="stars"></div>
+        <div id="stars2"></div>
+        <div id="stars3"></div>
 
-            <div class="sub-text">
-              <p>mubarak</p>
+        <div class="eid-greetings">
+          <div class="typography">
+            <div class="container-outer">
+              <div class="container">
+                <div class="main-text">
+                  <div class="top">
+                    <p>EID</p>
+                  </div>
+                  <div class="bottom">
+                    <p>EID</p>
+                  </div>
+
+                  <div class="sub-text">
+                    <p>mubarak</p>
+                  </div>
+                  <div class="date">
+                    <p>2023</p>
+                  </div>
+                </div>
+              </div>
             </div>
-            <div class="date">
-              <p>2023</p>
-            </div>
+          </div>
+          <div id="Doors" class="eid-greetings__Doors">
+            <div class="Doors__background"></div>
+            <div class="Doors__left"></div>
+            <div class="Doors__right"></div>
           </div>
         </div>
       </div>
-
       <footer>
         <p class="left">...جنيد</p>
       </footer>
